dc3b7a83e7adeef703fe8aec98c7c9641e75afd7
xolrax <carlos.alvear@coordinadorelectrico.cl>
2025-09-03 09:57:06 -0400

    Agregar operaciones de duplicación de columnas en el DataFrame en main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+from tkinter.messagebox import IGNORE
 import pandas as pd
 
 df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+# --- IGNORE ---
+
+df2 = pd.DataFrame() - -- IGNORE - --
+df2['A'] = df['A'] * 2
+df2['B'] = df['B'] * 2
